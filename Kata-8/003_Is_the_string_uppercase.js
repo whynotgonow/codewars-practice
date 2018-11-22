8357639b096c6a10765a31ef8a44ae9b52fbffe0
whynotgonow <whynotgonow@163.com>
2018-11-22 21:20:31 +0800

    kata8 003

---
+++ Kata-8/003_Is_the_string_uppercase.js
@@ -0,0 +1,38 @@
+/*
+Is the string uppercase?
+    Task
+    Create a method is_uppercase() to see whether the string is ALL CAPS. For example:
+    
+    is_uppercase("c") == False
+    is_uppercase("C") == True
+    is_uppercase("hello I AM DONALD") == False
+    is_uppercase("HELLO I AM DONALD") == True
+    is_uppercase("ACSKLDFJSgSKLDFJSKLDFJ") == False
+    is_uppercase("ACSKLDFJSGSKLDFJSKLDFJ") == True
+*/
+
+String.prototype.isUpperCase = function () {
+    let s = this.toString();
+    return !!(s && s.toUpperCase() === s)
+};
+
+console.log('C'.isUpperCase());
+
+
+/*
+note:
+    注意：此处需要调用实例上的方法，也就是需要this,this在箭头函数中没有意义
+*/
+/*
+solutions: 1) 2)
+    
+    String.prototype.isUpperCase = function () {
+        return this == this.toUpperCase()
+    };
+    
+    
+    String.prototype.isUpperCase = function() {
+        return this.toUpperCase() === this.toString();
+    }
+*/
+
